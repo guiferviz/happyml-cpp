4a89130a9eb17a8ac421ea0f68e8cb25b7dfe656
Guillermo Fernández <guiferviz@gmail.com>
2015-12-18 13:48:55 +0100

    Initial commit.

---
+++ include/happyml.h
@@ -0,0 +1,14 @@
+
+#ifndef _HAPPY_ML_H_
+#define _HAPPY_ML_H_
+
+
+namespace happyml
+{
+
+    void greet();
+
+}
+
+
+#endif // _HAPPY_ML_H_
---
+++ src/happyml.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+#include "happyml.h"
+
+
+using namespace std;
+
+
+namespace happyml
+{
+
+    string version = "HAPPY_ML_VERSION";
+
+
+    void greet()
+    {
+        cout << "Those about to learn we salute you." << endl;
+    }
+
+}
