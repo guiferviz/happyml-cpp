3ff45d36ba4b01e50e4fc71cb1092b3a318e716d
Guillermo Fernández <guiferviz@gmail.com>
2015-12-19 14:58:53 +0100

    Adding 'make doc' that generates doxygen documentation.

--- include/happyml.h
+++ include/happyml.h
@@ -9,12 +9,23 @@
 using namespace std;
 
 
+/**
+ * HappyML library namespace.
+ */
 namespace happyml
 {
 
+    /**
+     * String with the library version.
+     */
     const string version = HAPPY_ML_VERSION;
 
 
+    /**
+     * **Prints a greeting** to the standard output.
+     * This library it's not only a **happy** library,
+     * it's also a **polite** library :)
+     */
     void greet();
 
 }
